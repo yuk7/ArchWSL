49e5ef46caaec01e419528067104cb73ed487eea
yuk7 <yukx00@gmail.com>
2017-12-04 21:15:04 +0900

    add files

---
+++ env.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+ARCH=x86_64
+ROOTFS_VER=2017.11.01
+ROOTFS_FN=archlinux-bootstrap-${ROOTFS_VER}-${ARCH}.tar.gz
+ROOTFS_URL=http://mirrors.kernel.org/archlinux/iso/${ROOTFS_VER}/${ROOTFS_FN}
+
+GLIBC_BLD=17101600
+GLIBC_VER=2.26-4
+GLIBC_FN=glibc-wsl-${GLIBC_VER}-${ARCH}.pkg.tar.xz
+GLIBC_URL=https://github.com/yuk7/arch-prebuilt/releases/download/${GLIBC_BLD}/${GLIBC_FN}
+
+FRTCP_BLD=17101600
+FRTCP_VER=1.21-2
+FRTCP_FN=fakeroot-tcp-${FRTCP_VER}-${ARCH}.pkg.tar.xz
+FRTCP_URL=https://github.com/yuk7/arch-prebuilt/releases/download/${FRTCP_BLD}/${FRTCP_FN}
+
+LNCR_BLD=17120400
+LNCR_ZIP=icons.zip
+LNCR_FN=Arch.exe
+LNCR_URL=https://github.com/yuk7/WSL-DistroLauncher/releases/download/${LNCR_BLD}/${LNCR_ZIP}
+
+if type curl >/dev/null 2>&1 ;then
+    DLR="curl"
+fi
+if type wget >/dev/null 2>&1;then
+    DLR="wget"
+fi
+if type aria2c >/dev/null 2>&1; then
+    DLR="aria2c -x4"
+fi
+
+
---
+++ profile
@@ -0,0 +1,23 @@
+# /etc/profile
+
+#Set our umask
+umask 022
+
+# Load profiles from /etc/profile.d
+if test -d /etc/profile.d/; then
+	for profile in /etc/profile.d/*.sh; do
+		test -r "$profile" && . "$profile"
+	done
+	unset profile
+fi
+
+# Source global bash config
+if test "$PS1" && test "$BASH" && test -z ${POSIXLY_CORRECT+x} && test -r /etc/bash.bashrc; then
+	. /etc/bash.bashrc
+fi
+
+# Termcap is outdated, old, and crusty, kill it.
+unset TERMCAP
+
+# Man is much better than us at figuring this out
+unset MANPATH
