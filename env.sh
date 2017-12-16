24f479f1d9b434d41277f2af519c18e12259f098
yuk7 <yukx00@gmail.com>
2017-12-16 12:27:14 +0900

    env.sh :update fakeroot package

--- env.sh
+++ env.sh
@@ -10,8 +10,8 @@ GLIBC_VER="2.26-4"
 GLIBC_FN="glibc-wsl-${GLIBC_VER}-${ARCH}.pkg.tar.xz"
 GLIBC_URL="https://github.com/yuk7/arch-prebuilt/releases/download/${GLIBC_BLD}/${GLIBC_FN}"
 
-FRTCP_BLD="17101600"
-FRTCP_VER="1.21-2"
+FRTCP_BLD="17121600"
+FRTCP_VER="1.22-1"
 FRTCP_FN="fakeroot-tcp-${FRTCP_VER}-${ARCH}.pkg.tar.xz"
 FRTCP_URL="https://github.com/yuk7/arch-prebuilt/releases/download/${FRTCP_BLD}/${FRTCP_FN}"
 
