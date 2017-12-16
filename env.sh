02203e95884338cd700c1801b7d9386632428d8a
yuk7 <yukx00@gmail.com>
2017-12-16 13:14:12 +0900

    remove glibc-wsl package (bug was fixed in offical glibc package(=>2.26-7))

--- env.sh
+++ env.sh
@@ -5,11 +5,6 @@ ROOTFS_VER="2017.12.01"
 ROOTFS_FN="archlinux-bootstrap-${ROOTFS_VER}-${ARCH}.tar.gz"
 ROOTFS_URL="http://mirrors.kernel.org/archlinux/iso/${ROOTFS_VER}/${ROOTFS_FN}"
 
-GLIBC_BLD="17101600"
-GLIBC_VER="2.26-4"
-GLIBC_FN="glibc-wsl-${GLIBC_VER}-${ARCH}.pkg.tar.xz"
-GLIBC_URL="https://github.com/yuk7/arch-prebuilt/releases/download/${GLIBC_BLD}/${GLIBC_FN}"
-
 FRTCP_BLD="17121600"
 FRTCP_VER="1.22-1"
 FRTCP_FN="fakeroot-tcp-${FRTCP_VER}-${ARCH}.pkg.tar.xz"
