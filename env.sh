0dba8b53b1251a75c9f66e3cd5ef5d15db99ee38
yuk7 <yukx00@gmail.com>
2018-06-21 03:04:59 +0900

    env.sh: update Launcher to 18062100

--- env.sh
+++ env.sh
@@ -10,7 +10,7 @@ FRTCP_VER="1.22-1"
 FRTCP_FN="fakeroot-tcp-${FRTCP_VER}-${ARCH}.pkg.tar.xz"
 FRTCP_URL="https://github.com/yuk7/arch-prebuilt/releases/download/${FRTCP_BLD}/${FRTCP_FN}"
 
-LNCR_BLD="18060400"
+LNCR_BLD="18062100"
 LNCR_ZIP="icons.zip"
 LNCR_FN="Arch.exe"
 LNCR_URL="https://github.com/yuk7/WSL-DistroLauncher/releases/download/${LNCR_BLD}/${LNCR_ZIP}"
