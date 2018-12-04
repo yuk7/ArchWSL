526ad63080dd9d8f93028056eebbba5032b0a080
hdk5 <hdk5@hdk5.xyz>
2018-12-05 00:42:53 +0300

    Add build.sh warning

--- build.sh
+++ build.sh
@@ -1,5 +1,9 @@
 #!/bin/bash -x
 
+# Warning: this script is not supposed to be run on real system,
+# as it will clean up after itself.
+# Use it only in ephemeral environments, such as Travis CI.
+
 source ./env.sh
 
 curl -L ${ISO_URL} -o ${ISO_FN}
