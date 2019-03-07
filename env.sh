e3d720fd0b586e2faa87c4c81b71da23bb75809f
yuk7 <yukx00@gmail.com>
2019-03-07 18:52:54 +0900

    Update Launcher to 19030700

--- env.sh
+++ env.sh
@@ -5,7 +5,7 @@ ROOTFS_VER="19022800"
 ROOTFS_FN="rootfs.tar.gz"
 ROOTFS_URL="https://github.com/yuk7/ArchWSL-FS/releases/download/${ROOTFS_VER}/${ROOTFS_FN}"
 
-LNCR_BLD="19022300"
+LNCR_BLD="19030700"
 LNCR_ZIP="icons.zip"
 LNCR_FN="Arch.exe"
 LNCR_URL="https://github.com/yuk7/wsldl/releases/download/${LNCR_BLD}/${LNCR_ZIP}"
