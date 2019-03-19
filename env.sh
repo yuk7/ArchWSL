804261cf4aece12bcd085ae9a00a2c03318c05c2
yuk7 <yukx00@gmail.com>
2019-03-19 11:22:53 +0900

    Update wsldl & Release 19.3.19.1

--- env.sh
+++ env.sh
@@ -5,7 +5,7 @@ ROOTFS_VER="19030700"
 ROOTFS_FN="rootfs.tar.gz"
 ROOTFS_URL="https://github.com/yuk7/ArchWSL-FS/releases/download/${ROOTFS_VER}/${ROOTFS_FN}"
 
-LNCR_BLD="19031900"
+LNCR_BLD="19031901"
 LNCR_ZIP="icons.zip"
 LNCR_FN="Arch.exe"
 LNCR_URL="https://github.com/yuk7/wsldl/releases/download/${LNCR_BLD}/${LNCR_ZIP}"
